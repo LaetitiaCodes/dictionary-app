7fda1604b9bcf151e395257550d6e5a4e973b919
Laetitia <88789728+LaetitiaCodes@users.noreply.github.com>
2022-03-31 12:43:55 +0200

    fixed warning (replaced <p> by a <div>)

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,6 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -10,13 +9,13 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             {" "}
-            <p>
+            <div>
               <strong>Definition:</strong>
               {definition.definition}
               <br /> <strong>Example:</strong>
               <em>{definition.example}</em>
               <Synonyms synonyms={definition.synonyms} />
-            </p>
+            </div>
           </div>
         );
       })}

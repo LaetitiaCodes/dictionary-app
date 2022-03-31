1ae1ff44f0feb4cab6f74c5f0dc61dbb37e47be7
Laetitia <88789728+LaetitiaCodes@users.noreply.github.com>
2022-03-31 13:57:38 +0200

    added styling

--- src/Photos.js
+++ src/Photos.js
@@ -6,20 +6,22 @@ export default function Photos(props) {
     return (
       <section className="Photos">
         <div className="row">
-          {props.photos.map(function (photo, index) {
-            return (
-              <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img
-                    src={photo.src.landscape}
-                    className="img-fluid"
-                    alt="keyword_photo"
-                  />{" "}
-                </a>
-              </div>
-            );
-          })}
-        </div>{" "}
+          <div className="col-3">
+            {props.photos.map(function (photo, index) {
+              return (
+                <span key={index}>
+                  <a href={photo.src.original} target="_blank" rel="noreferrer">
+                    <img
+                      src={photo.src.landscape}
+                      className="img-fluid"
+                      alt="keyword_photo"
+                    />
+                  </a>
+                </span>
+              );
+            })}
+          </div>
+        </div>
       </section>
     );
   } else {

2a2c2c1546fd96a619a8ad0c76c32185c1e3fc85
Laetitia <88789728+LaetitiaCodes@users.noreply.github.com>
2022-03-31 11:54:29 +0200

    added default keyword on load

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,24 +4,28 @@ import Photos from "./Photos";
 import Results from "./Results.js";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  const [keyword, setKeyword] = useState(" ");
+export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
   const [photos, setPhotos] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
   function handlePexelsResponse(response) {
     setPhotos(response.data.photos);
   }
 
-  function search(event) {
+  function handleSubmit(event) {
     event.preventDefault();
+    search();
+  }
+
+  function search(keyword) {
     //Link to API Documentation: https://dictionaryapi.dev/
+
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
 
     const pexelsApiKey =
@@ -34,15 +38,30 @@ export default function Dictionary() {
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+  function load() {
+    setLoaded(true);
+    search();
+  }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search}>
-          <input type="search" onChange={handleKeywordChange} />
-        </form>{" "}
-      </section>
-      <Results results={results} /> <Photos photos={photos} />{" "}
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Enter a word, e.g. sunset, yoga, snow..."
+              autoFocus="on"
+              autoComplete="off"
+              onChange={handleKeywordChange}
+            />
+          </form>{" "}
+        </section>
+        <Results results={results} /> <Photos photos={photos} />{" "}
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
